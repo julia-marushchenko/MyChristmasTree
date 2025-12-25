b4f38740c8f717668e69455e39a78fa6bd12f529
Julia Marushchenko <marushchenko.yulia@gmail.com>
2025-12-25 18:02:45 +0100

    Add files via upload
    Committing Main class.

---
+++ src/com/mytree/Main.java
@@ -0,0 +1,28 @@
+/**
+ * Java program to print a Christmas tree.
+ */
+
+package com.mytree;
+
+/**
+ * Main class
+ */
+public class Main {
+
+    // Main method to run java program
+    public static void main(String[] args) {
+
+        System.out.println("Christmas Tree!");
+
+        System.out.println("      *     ");
+        System.out.println("     ***    ");
+        System.out.println("    *****   ");
+        System.out.println("   *******   ");
+        System.out.println("  *********   ");
+        System.out.println(" ***********  ");
+        System.out.println("      **    ");
+        System.out.println("    ######   ");
+        System.out.println("    ######   ");
+        System.out.println("    ######   ");
+    }
+}
